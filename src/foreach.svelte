ee31071214ab47fc903777a07834d348e99efb00
grooveT <grooveturtle29@gmail.com>
2022-10-17 02:42:45 +0900

    forech test

---
+++ src/foreach.svelte
@@ -0,0 +1,45 @@
+<script>
+    let list = [
+        {
+            id: 0,
+            content: '첫 번째 할 일',
+            done: false
+        },
+        {
+            id: 1,
+            content: '두 번째 할 일',
+            done: false
+        },
+        {
+            id: 2,
+            content: '세 번째 할 일',
+            done: true
+        },
+        {
+            id: 3,
+            content: '네 번째 할 일',
+            done: false
+        }
+    ]
+
+    const handleAddTodo = () => {
+        const todo = {
+            id: list.length,
+            content: '새로운 할 일',
+            done: false
+        }
+
+        list = [...list, todo];
+    }
+</script>
+<ul>
+    {#each list as item}
+    <li>
+        <p>{item.id}</p>
+        <p>{item.content}</p>
+        <p>{item.done}</p>
+    </li>
+    {/each}
+</ul>
+
+<button on:click="{handleAddTodo}">할 일 추가</button>
