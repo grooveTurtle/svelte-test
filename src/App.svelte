bd058226ee52448b477ab88d9bcdf2005d477d3b
grooveT <grooveturtle29@gmail.com>
2022-10-17 22:59:09 +0900

    handleMousemove

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,12 @@
 <script>
 //  import Foreach from './foreach.svelte';
 	// import HandleClick from './handleClick.svelte';
-	import HandleMousemove from './handleMousemove.svelte';
+	// import HandleMousemove from './handleMousemove.svelte';
+	// import EventModifier from './eventModifier.svelte';
+    import EventModifiers from './eventModifiers.svelte';
 </script>
 
 <!-- <Foreach /> -->
 <!-- <HandleClick /> -->
-<HandleMousemove />
+<!-- <HandleMousemove /> -->
+<EventModifiers />
